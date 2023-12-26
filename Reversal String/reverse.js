617f90ea409ae89cd6207908ba1123afe0a9929a
Harshchauhan29626 <150172292+Harshchauhan29626@users.noreply.github.com>
2023-12-26 16:24:59 +0530

    Add files via upload

---
+++ Reversal String/reverse.js
@@ -0,0 +1,9 @@
+function reverse() {
+    var num1 = document.getElementById("num1").value;
+
+
+    document.getElementById("p1").innerHTML +=  "Reversed String: " +(num1.split("").reverse().join(""));
+
+
+    // console.log(num1.split("").reverse().join(""));
+}
